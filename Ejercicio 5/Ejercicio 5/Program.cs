e782c555b7fc34e6f02f739b06bda09dd2ae80c7
CarlosFdezJim <carlosfdezjim@gmail.com>
2020-10-12 12:18:28 +0200

    Ejercicios C#

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 20);
+            bool acertado = false;
+
+            Console.WriteLine("Escribe un número aleatorio entre 0 y 20 (inclusives) ");
+
+
+            for (int i = 0; i < 5 && acertado != true; i++)
+            {
+                int entrada = int.Parse(Console.ReadLine());
+
+                if (num > entrada)
+                {
+                    Console.WriteLine("Introduzca un número MAYOR");
+                }
+                else if (num < entrada)
+                {
+                    Console.WriteLine("Introduzca un número MENOR");
+                }
+                else
+                {
+                    Console.WriteLine("!!!HAS ACERTADOO!!!");
+                    acertado = true;
+                }
+            }
+
+            if (acertado == false)
+            {
+                Console.WriteLine("El número era " + num);
+            }
+
+
+        }
+    }
+}
---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Ejercicio6
+{
+    public static void Main()
+    {
+        Console.WriteLine("Escribe un número y descubre cuantos 7 hay en él.");
+        //int num = 
+        Console.WriteLine("247371");
+        Console.WriteLine("---------------------------------------");
+        int count = 0;
+        int num = 247371;
+
+        //var number = new List <String>();
+        //number.Add(num);
+
+        //foreach --> String
+        //do...while -->Int
+
+        foreach (int x in num)
+        {
+            if (num.Equals(7))
+            {
+                count++;
+                Console.WriteLine(x);
+            }
+            
+        }
+
+        Console.WriteLine("El número 7 se ha repetido " + count + " veces");
+        //Console.ReadKey();
+    }
+}
