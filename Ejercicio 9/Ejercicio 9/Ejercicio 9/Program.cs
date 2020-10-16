35f5ba9b3702e87f1734a1b2bb223cecf9e4406a
Carlos Fdez <carlosfdezjim@gmail.com>
2020-10-16 19:46:13 +0200

    Segunda Entrega Ejercicios C#

--- Ejercicio 9/Ejercicio 9/Ejercicio 9/Program.cs
+++ Ejercicio 9/Ejercicio 9/Ejercicio 9/Program.cs
@@ -6,17 +6,32 @@ namespace Ejercicio_9
     {
         static void Main(string[] args)
         {
-            int num1 = 2;
-            int num2 = 0;
+            int num1;
+            int num2;
+
+            
 
 
             try
             {
+                Console.WriteLine("Introduzca dos números para dividirlos.");
+                Console.WriteLine("Introduzca un número");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Introduzca otro número");
+                num2 = int.Parse(Console.ReadLine());
                 int result = num1 / num2;
             }
-            catch ( Exception e)
+            catch ( DivideByZeroException )
             {
                 Console.WriteLine("No se puede realizar una división entre 0");
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine("El valor debe de ser un valor entero.");
+            }
+            catch (OverflowException )
+            {
+                Console.WriteLine("El número es demasiado grande");
             }
         }
     }
